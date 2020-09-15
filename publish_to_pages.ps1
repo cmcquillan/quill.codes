da3df86154d94227fd45b1117d29cd54b68ccc03
Casey McQuillan <Casey@quilltechnologies.com>
2020-09-15 09:06:39 -0700

    Rewriting to powershell.

--- publish_to_pages.ps1
+++ publish_to_pages.ps1
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
+if [ "`git status -s`" ];
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
@@ -21,8 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "Changing to public directory"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
+echo "Pushing to github"
 git push --all
