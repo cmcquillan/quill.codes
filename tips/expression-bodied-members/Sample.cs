d72e4c00b97b5170bd3dbbd84abcdd7945c6524c
Casey McQuillan <Casey@quilltechnologies.com>
2020-10-17 13:58:01 -0700

    Publishing to gh-pages (publish_to_pages.ps1)

--- tips/expression-bodied-members/Sample.cs
+++ tips/expression-bodied-members/Sample.cs
@@ -1,8 +1,6 @@
-public class Cake
-{
-  private readonly ISprinkles _sprinkles;
-  private readonly IFrosting _frosting;
 
-  public Cake(ISprinkles sprinkles, IFrosting frosting)
-    => (_sprinkles, _frosting) = (sprinkles, frosting);
+
+public static class ExpressionBodies
+{
+    public string Feature => "👍";
 }
