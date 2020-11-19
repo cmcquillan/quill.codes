93bf643b35bc201de0260246bce13f131b6e0929
Casey McQuillan <Casey@quilltechnologies.com>
2020-11-18 20:37:20 -0800

    Adding fiddle.

--- netlify_publish.sh
+++ netlify_publish.sh
@@ -3,6 +3,10 @@
 npm install -g postcss postcss-cli
 npm install -g autoprefixer
 
+cd assets/apps/fiddle/src/Fiddle.Run.Client
+ng build --prod --outputPath=../../../../../content/apps/fiddle --baseHref=/apps/fiddle/ --deployUrl=/apps/fiddle/ --deleteOutputPath=false
+cd ../../../../..
+
 cd themes/quill-codes/
 
 npm install
