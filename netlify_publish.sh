34841ae973942b43624dd77a3a86535880ca27f2
Casey McQuillan <Casey@quilltechnologies.com>
2020-11-18 20:39:35 -0800

    npm install for fiddle directory.

--- netlify_publish.sh
+++ netlify_publish.sh
@@ -4,6 +4,7 @@ npm install -g postcss postcss-cli
 npm install -g autoprefixer
 
 cd assets/apps/fiddle/src/Fiddle.Run.Client
+npm install
 ng build --prod --outputPath=../../../../../content/apps/fiddle --baseHref=/apps/fiddle/ --deployUrl=/apps/fiddle/ --deleteOutputPath=false
 cd ../../../../..
 
